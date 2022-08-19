bbd0802e94562f77edc42c85a2513413c62c6269
Khatri7968 <engrpawan111@gamil.com>
2022-08-19 05:18:22 -0700

    edited

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -5,38 +5,42 @@ import numpy as np
 
 # Import data
 df = pd.read_csv("medical_examination.csv")
-print(df)
+
 
 # Add 'overweight' column
 # Add an overweight column to the data. To determine if a person is overweight, first calculate their BMI by dividing
 # their weight in kilograms by the square of their height in meters. If that value is > 25 then the person is overweight.
 # Use the value 0 for NOT overweight and the value 1 for overweight.
 
-overweight = df['weight']/(df['height']*df['height']/10000)
+weight = df['weight']/(df['height']*df['height']/10000)
 
-df['overweight'] = overweight > 25
+df['overweight'] = weight > 25
 
 df.replace({False: 0, True: 1}, inplace=True)
 
-print(df["overweight"])
 
 
+# Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0.
+# If the value is more than 1, make the value 1.
 
-# Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
+df['cholesterol'] = np.where(df['cholesterol'].values > 1, 1, 0)
 
+df['gluc'] = np.where(df['gluc'].values > 1, 1, 0)
 
 # Draw Categorical Plot
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat = None
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars= ['cholestrol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
-    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
-    df_cat = None
+    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature.
+    # You will have to rename one of the columns for the catplot to work correctly.
+    df_cat["total"] = 1
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value'], as_index =False).count()
 
     # Draw the catplot with 'sns.catplot()'
 
-    # Get the figure for the output
-    fig = None
+    sns.set_theme(style='darkgrid')
+    fig = sns.catplot(x="variable", y="total", data=df_cat, hue="value", kind="bar", col='cardio').fig
 
     # Do not modify the next two lines
     fig.savefig('catplot.png')
@@ -64,5 +68,5 @@ def draw_heat_map():
     return fig
 
 if __name__ == "__main__":
-    pass
+    draw_cat_plot()
 
